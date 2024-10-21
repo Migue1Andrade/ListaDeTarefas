a365aeb9dcf0748088d32f746fe120078d99f25b
Migue1Andrade <mfrancadeandrade@gmail.com>
2024-10-21 09:54:35 -0300

    final commit

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,73 +1,95 @@
 const inputTask = document.querySelector('.input');
-const addButton = document.querySelector('.add-button'); 
-const taskList = document.querySelector('.tasks'); 
+const addButton = document.querySelector('.add-button');
+const taskList = document.querySelector('.tasks');
 
-function cleanInput() {
-	inputTask.value = '';
-	inputTask.focus(); 
-	console.log(inputTask.value);
-}
+// cria os botoes e os li's com as tasks
+function makeTask(string) {
+	let li = document.createElement('li');
+	const text = document.createElement('p');
+	text.textContent = string;
+	li.appendChild(text);
 
-function buidRemoveBotton(li) {
-	li.innerText += ' ';
-	const deleteButton = document.createElement('button'); 
-	deleteButton.innerText = 'remover';
+	taskList.appendChild(li); 
+	const deleteButton = document.createElement('button');
+	deleteButton.textContent = 'remover';
 	deleteButton.setAttribute('class', 'del-button');
 	deleteButton.setAttribute('title', 'delete this task');
 	li.appendChild(deleteButton);
-}
-
-function makeTask(string) { 
-	const li = document.createElement('li');
-	li.innerText = string;
-	taskList.appendChild(li); 
+	const checkButton = document.createElement('button');
+	checkButton.textContent = 'check';
+	checkButton.setAttribute('class', 'check-button');
+	checkButton.setAttribute('title', 'editar esse botao');
+	li.appendChild(checkButton);
 	cleanInput();
-	buidRemoveBotton(li);
-	saveTask(); 
+	saveTask();
 };
 
+// recebe evento do botao adicionar
+addButton.addEventListener('click', function(event) {
+	if(!inputTask.value) return;
+
+makeTask(inputTask.value);
+});
+
+// cria a funcionalidade que permite a chamada da funcao maker a partir do enter
 inputTask.addEventListener('keypress', function(event) {
 	if(event.keyCode === 13) {
-		if(!inputTask.value) return; 
-		makeTask(inputTask.value); 
+		if(!inputTask.value) return;
+		makeTask(inputTask.value);
 	};
 });
 
-addButton.addEventListener('click', function(event) {
-	if(!inputTask.value) return; 
+// cria a funcao de limpar o input
+function cleanInput() {
+	inputTask.value = '';
+	inputTask.focus();
+	console.log(inputTask.value);
+}
 
-makeTask(inputTask.value); 
-});
-
+// faz a remocao e chagem de cada list
 document.addEventListener('click', function(event) {
-	const el = event.target; 
+	const li = event.target;
 	
-	if(el.classList.contains('del-button')) {
-		el.parentElement.remove();
+	if(li.classList.contains('del-button')) {
+		li.parentElement.remove();
 		saveTask();
 	}
+	else if(li.classList.contains('check-button')) {
+
+		if(li.parentElement.children[2].textContent === 'check'){
+			li.parentElement.children[2].textContent = 'cheked';
+			li.parentElement.children[0].style.textDecoration = "line-through";
+		}
+		else {
+			console.log('Nao checado', li.parentElement.children[2].textContent);
+			li.parentElement.children[2].textContent = 'check';
+			li.parentElement.children[0].style.textDecoration = "none";
+		};
+		
+	};
 });
 
+// salva tudo no localStorage do navegador
 function saveTask() {
-	const liTarefas = taskList.querySelectorAll('li');
+	const liTasks = taskList.querySelectorAll('li');
 	const listaDeTarefas = [];
 
-	for (let tarefa of liTarefas ) {
+	for (let tarefa of liTasks) {
 		let tarefaTexto = tarefa.innerText;
 		tarefaTexto = tarefaTexto.replace('remover', '').trim();
 		listaDeTarefas.push(tarefaTexto);
-	}
+	};
 
 	const tarefasJSON = JSON.stringify(listaDeTarefas);
 	localStorage.setItem('tarefas', tarefasJSON); 
-}
-
+};
+// adiciona as tasks salvas no make dnv
 function addTaskSaved() {
-	const tarefas = localStorage.getItem('tarefas'); 
+	const tarefas = localStorage.getItem('tarefas');
 	const listaDeTarefas = JSON.parse(tarefas);
 
 	for (let tarefa of listaDeTarefas){
 		makeTask(tarefa);
-	}
-}
-addTaskSaved(); 
+	};
+};
+addTaskSaved();
